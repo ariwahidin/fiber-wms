156a42d313b0f8ca859ff18fa3b41610082500c3
ari.wahidin <ariwahidin21@gmail.com>
2026-09-24 23:16:29 +0700

    fix sto

--- models/stock_take_model.go
+++ models/stock_take_model.go
@@ -6,8 +6,16 @@ import (
 	"gorm.io/gorm"
 )
 
+type BaseModel struct {
+	ID        uint           `json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
+
 type StockTakeBatch struct {
-	gorm.Model
+	// gorm.Model
+	BaseModel
 
 	Code        string     `json:"code" gorm:"unique;not null"`
 	OwnerCode   string     `json:"owner_code"`
